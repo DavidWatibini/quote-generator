e80446b860d53211d122c52d6d3e9e54d714e97b
Watinguyaz <watimakhanu@gmail.com>
2019-01-15 11:42:11 +0300

    final page stylings done

--- quote-generator/src/app/quote.ts
+++ quote-generator/src/app/quote.ts
@@ -1,6 +1,4 @@
 export class Quote{
-  public showQuote:boolean;
   constructor(public id:number, public publisher:string, public author:string, public quote:string){
-    this.showQuote=false
   }
 }
